49e32694e1d9f555ecac8596e4c88db7496a19ea
xing <172176234@qq.com>
2020-12-15 16:29:47 +0800

    update

--- src/service/index.js
+++ src/service/index.js
@@ -3,3 +3,7 @@ import request from '@/utils/request';
 export function queryList(params) {
   return request.get('/example/list', { params });
 }
+
+export function post(params) {
+  return request('/example/list', { params, method: 'POST' });
+}
